a6f3b40f3db9a38a0b380fbfbcbb2f9086bd4343
Cem-Atlas <cematlas00@gmail.com>
2024-07-13 01:10:23 +0300

    a

---
+++ js/task-1.js
@@ -0,0 +1,7 @@
+const quantity = (5);
+const pricePerDroid = (3000);
+const totalPrice = (quantity * pricePerDroid);
+const makeTransaction = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+console.log(makeTransaction);
+
+
